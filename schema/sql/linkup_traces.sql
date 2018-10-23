e31ae493e16b157c854e2948b2c3fe80dd297bf3
Mei-Hui Su <mei@MeiPro.local>
2018-10-23 08:54:03 -0700

    take out unused table entry in object_tb

--- schema/sql/linkup_traces.sql
+++ schema/sql/linkup_traces.sql
@@ -2,7 +2,7 @@ CREATE TEMP TABLE tmp_x AS
      SELECT name, concat(name,'-trace'), gid from OBJECT_tb;
 
 UPDATE OBJECT_tb 
-    SET (TRACE_tb_gid, trace_geojson) = (TRACE_tb.gid, ST_AsGeoJSON(TRACE_tb.geom))
+    SET (TRACE_tb_gid) = (TRACE_tb.gid)
     FROM tmp_x, TRACE_tb
     WHERE TRACE_tb.layer = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
