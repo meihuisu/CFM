a2c9ee022bf945d97cc496e89a52fb72281fc7f7
Mei-Hui Su <mei@MeiPro.local>
2018-11-26 11:40:43 -0800

    update linkup_traces.sql

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # CFM
 Community Fault Model
+
+
+Setting up on linux node: 
+
+yum install git
+yum -y install gcc
+yum -y install readline-devel
+yum -y install zlib-devel
+
+wget https://ftp.postgresql.org/pub/source/v10.5/postgresql-10.5.tar.gz
+tar -xzf postgresql-10.5.tar.gz
+cd postgresql-10.5
+./configure --prefix=/usr/local
+make
+sudo make install
+
+echo "Update postgres user  .. "
+sudo /usr/sbin/userdel postgres 
+##scec
+sudo /usr/sbin/useradd -d /home/postgres -m postgres
+read -s -p "Enter password for postgres: " password
+echo $password |sudo passwd --stdin postgres
+sudo chage postgres -M -1
+
+echo "Adding cfmuser .. "
+##scec
+sudo /usr/sbin/useradd -d /home/cfmuser -m cfmuser
+read -s -p "Enter password for cfmuser: " password
+echo $password |sudo passwd --stdin cfmuser
+sudo chage cfmuser -M -1
+
+##
+su - postgres
+initdb /home/postgres/cfm_db
+pg_ctl -D /home/postgres/cfm_db -l logfile start
+createuser --interactive
+
+## 
+su - cfmuser
+cd 
+git clone https://github.com/meihuisu/CFM.git
+
+
+
+
--- schema/sql/linkup_traces.sql
+++ schema/sql/linkup_traces.sql
@@ -4,7 +4,7 @@ CREATE TEMP TABLE tmp_x AS
      SELECT name, concat(name,'-trace'), gid from OBJECT_tb;
 
 UPDATE OBJECT_tb 
-    SET (TRACE_tb_gid) = (TRACE_tb.gid)
+    SET TRACE_tb_gid = TRACE_tb.gid
     FROM tmp_x, TRACE_tb
     WHERE TRACE_tb.layer = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
@@ -12,10 +12,10 @@ UPDATE OBJECT_tb
 DROP TABLE tmp_x;
 
 CREATE TEMP TABLE tmp_x AS
-     SELECT name, concat(name,'-m1000'), gid from OBJECT_tb;
+     SELECT name, concat(name,'_m1000'), gid from OBJECT_tb;
 
 UPDATE OBJECT_tb 
-    SET (OBJECT_1000m_tb_gid) = (OBJECT_1000m_tb.gid)
+    SET OBJECT_1000m_tb_gid = OBJECT_1000m_tb.gid
     FROM tmp_x, TRACE_tb
     WHERE OBJECT_10000m_tb.name = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
@@ -23,10 +23,10 @@ UPDATE OBJECT_tb
 DROP TABLE tmp_x;
 
 CREATE TEMP TABLE tmp_x AS
-     SELECT name, concat(name,'-m500'), gid from OBJECT_tb;
+     SELECT name, concat(name,'_m500'), gid from OBJECT_tb;
 
 UPDATE OBJECT_tb 
-    SET (OBJECT_500m_tb_gid) = (OBJECT_500m_tb.gid)
+    SET OBJECT_500m_tb_gid = OBJECT_500m_tb.gid
     FROM tmp_x, TRACE_tb
     WHERE OBJECT_500m_tb.name = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
@@ -34,10 +34,10 @@ UPDATE OBJECT_tb
 DROP TABLE tmp_x;
 
 CREATE TEMP TABLE tmp_x AS
-     SELECT name, concat(name,'XXX'), gid from OBJECT_tb;
+     SELECT name, concat(name,''), gid from OBJECT_tb;
 
 UPDATE OBJECT_tb 
-    SET (OBJECT_native_tb_gid) = (OBJECT_native_tb.gid)
+    SET OBJECT_native_tb_gid = OBJECT_native_tb.gid
     FROM tmp_x, TRACE_tb
     WHERE OBJECT_native_tb.name = tmp_x.concat
     AND tmp_x.gid = OBJECT_tb.gid;
